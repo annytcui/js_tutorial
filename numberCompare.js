713cb8d6f44a24918653f2802be9d5070da3d71e
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-04-22 17:17:16 +0800

    Add a function

---
+++ numberCompare.js
@@ -0,0 +1,13 @@
+let a = [42, 17, 8, 99];
+
+function numberCompare(a, b) {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+console.log(a.sort(numberCompare));
