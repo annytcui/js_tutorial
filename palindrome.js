63300354e21af12bb072627e58c8269e34abe97b
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-04-24 19:07:38 +0800

    Construct a second object with prototype

--- palindrome.js
+++ palindrome.js
@@ -1,20 +1,36 @@
-// Reverse a string
+// Reverses a string.
 function reverse(string) {
   return Array.from(string).reverse().join("");
 }
 
-// Defines a Phrase object
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  // Makes the phrase LOUDER.
-  this.louder = function() {
-    return this.content.toUpperCase();
-  };
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === reverse(this.processedContent());
   };
 }
+
+// Defines a translated Phrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
